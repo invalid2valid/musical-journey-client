bd51f131851bbb0bc6e7eb4bf40757463a3b26da
Hazrat Ali <hazratali@Hazrats-MacBook-Air.local>
2023-06-07 12:19:04 +0600

    errorpage done

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div className="text-center">error</div>;
+  return (
+    <div className="text-center w-full flex justify-center items-center">
+      <div className="p-10 flex flex-col items-center">
+        <h1 className="text-4xl font-bold  text-center">
+          Ohhh.. Something is wrong
+        </h1>
+        <img className=" my-5" src="../../../src/assets/Sketch.png" alt="" />
+
+        <Link
+          to={"/"}
+          className="border-2 p-5 border-orange-400 rounded-lg shadow-lg"
+        >
+          {" "}
+          Go to Home{" "}
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
